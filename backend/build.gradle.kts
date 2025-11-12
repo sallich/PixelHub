ee9ddf9931e6cbc873872d05f79167bf08662d06
Ray_1024 <skovorodnikovda@mail.ru>
2025-11-12 14:34:10 +0300

    add spring boot actuator to backend health checking

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
     implementation("org.liquibase:liquibase-core")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
